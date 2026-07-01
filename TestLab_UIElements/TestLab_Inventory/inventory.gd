f82b8d839cf0597d51f5d8cf6268ec5c46252aeb
MenInHats <ecg0975887@gmail.com>
2026-06-30 21:57:03 -0500

    Added Inventory Test Lab
    Added
    Inventory, a resource for objects that will require an inventory system
    Inventory Item, a resource for available items to be used in the inventory system
    PhCircleItem, a test item for the inventory system
    test_inventory, a teesting inventory for the inventory system

---
+++ TestLab_UIElements/TestLab_Inventory/inventory.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name Inv
+
+# ----------------------
+# Created 6/30/2026
+# Purpose: A resources to give to different objects that will need a inventory function
+#
+# ----------------------
+
+#items within the inventory
+@export var items: Array[InvItem]
---
+++ TestLab_UIElements/TestLab_Inventory/inventory_item.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name InvItem
+
+# ----------------------
+# Created 6/30/2026
+# Purpose: The resource for abailable items in the game for the inventory
+#
+# ----------------------
+
+#properties of any inventory item
+@export var name: String = ""
+@export var texture: Texture2D
